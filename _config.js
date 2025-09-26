5cabf3ffa6c3cccf7f90d1a0b13486b8fb191a2c
Waithera-Macharia-J <joycemacharia074@gmail.com>
2025-09-26 13:17:42 +0300

    Add MILESTONE 2 banner to landing page

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
+// MongoDB Atlas connection strings
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://ceradb:ceradb@jenkins-project.djgfqg1.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Jenkins-Project',
+    development: 'mongodb+srv://ceradb:ceradb@jenkins-project.djgfqg1.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Jenkins-Project',
+    test: 'mongodb+srv://ceradb:ceradb@jenkins-project.djgfqg1.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Jenkins-Project',
 }
+
 module.exports = config;
--- server.js
+++ server.js
@@ -2,45 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const config = require('./_config');  // <-- import your config
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-});
+// Determine environment (production, development, test)
+const env = process.env.NODE_ENV || 'development';
+const dbURI = config.mongoURI[env];
 
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+// Connect to MongoDB Atlas
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Initializing the app
 const app = express();
 
-
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body parser middleware
-app.use(express.json())
+app.use(express.json());
 
-
+// Routes
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
+// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
 });
